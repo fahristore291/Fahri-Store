f703a2223e3238406dce2aa1a7e2f8eed60de655
fahristore291 <magter326@gmail.com>
2025-12-18 22:50:11 +0700

    Update data-nominal.js

--- data-nominal.js
+++ data-nominal.js
@@ -1,1 +1,75 @@
+const nominalData = {
 
+  ml: [
+    "5 Diamonds",
+    "10 Diamonds",
+    "14 Diamonds",
+    "28 Diamonds",
+    "42 Diamonds",
+    "70 Diamonds",
+    "86 Diamonds",
+    "140 Diamonds",
+    "172 Diamonds",
+    "257 Diamonds",
+    "284 Diamonds",
+    "344 Diamonds",
+    "429 Diamonds",
+    "514 Diamonds",
+    "600 Diamonds",
+    "706 Diamonds",
+    "878 Diamonds",
+    "1412 Diamonds",
+    "1708 Diamonds",
+    "2195 Diamonds",
+    "2398 Diamonds",
+    "2976 Diamonds",
+    "3688 Diamonds",
+    "Weekly Diamond Pass"
+  ],
+
+  ff: [
+    "5 Diamonds",
+    "12 Diamonds",
+    "50 Diamonds",
+    "70 Diamonds",
+    "140 Diamonds",
+    "210 Diamonds",
+    "355 Diamonds",
+    "500 Diamonds",
+    "720 Diamonds",
+    "1000 Diamonds",
+    "1450 Diamonds",
+    "2180 Diamonds",
+    "3640 Diamonds"
+  ],
+
+  pubg: [
+    "60 UC",
+    "120 UC",
+    "180 UC",
+    "325 UC",
+    "660 UC",
+    "1800 UC",
+    "3850 UC",
+    "8100 UC"
+  ],
+
+  genshin: [
+    "60 Genesis Crystal",
+    "300 + Bonus",
+    "980 + Bonus",
+    "1980 + Bonus",
+    "3280 + Bonus",
+    "6480 + Bonus",
+    "Blessing of the Welkin Moon"
+  ],
+
+  mcgg: [
+    "Weekly Pass",
+    "Monthly Pass",
+    "Joki Rank Epic",
+    "Joki Rank Legend",
+    "Joki Rank Mythic"
+  ]
+
+};
